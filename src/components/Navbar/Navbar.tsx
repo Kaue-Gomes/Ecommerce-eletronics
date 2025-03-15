f619e96003ae027fdb68668b447231713fde5281
Kaue-Gomes <kauegomessales189@gmail.com>
2025-03-14 22:51:07 -0300

    style nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,35 @@
-import styles from "..scss/Navbar.module.css";
+import styles from "../../scss/Navbar.module.scss";
 import { FaBars, FaSearch, FaShoppingCart } from "react-icons/fa";
 import Navigation from "./Navigation";
 
+
 const Navbar = () => {
   return (
-    <div className="nav">
-      <h1 className="logo">
-        ElETRO<span>STORE</span>
+    
+    <div className={styles.nav}>
+      <div className={styles["inner-content"]}>
+      <h1 className={styles.logo}>
+        ELETRO<span>STORE</span>
       </h1>
       <nav>
         <ul>
           <Navigation />
         </ul>
       </nav>
-      <div className="navs-icon-container">
-        <div className="search-input-container">
-          <input type="search" />
+      <div className={styles["navs-icon-container"]}>
+        <div className={styles["search-input-container"]}>
+          <input placeholder="procurar" type="search" />
           <FaSearch />
         </div>
-        <button className="shopping-cart">
+        <button className={styles["shopping-cart"]}>
           <FaShoppingCart />
-          <div className="products-count">0</div>
+          <div className={styles["products-count"]}>0</div>
         </button>
-        <button className="menu-button">
+        <button className={styles["menu-button"]}>
           <FaBars />
         </button>
       </div>
-     
+    </div>
     </div>
   );
 };
