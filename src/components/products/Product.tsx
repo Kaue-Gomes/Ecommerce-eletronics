c66cc36e5a61446dfc2809fbb35d0e6fc2b85036
Kaue-Gomes <kauegomessales189@gmail.com>
2025-03-15 12:34:13 -0300

    styles

--- src/components/products/Product.tsx
+++ src/components/products/Product.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { ProductType } from '../../types/types'; 
 import { Link } from 'react-router-dom';
 import { FaMoneyBill,FaCartShopping } from 'react-icons/fa6';
-
+import styles from '../../scss/Products.module.scss';
 
 
 const Product: React.FC<ProductType> = ({ id, name, price, image, rating }) => {
   return (
-    <div className="product">
+    <div className={styles["product"]}>
       <img src={image} alt={name} />
       <h3>{name}</h3>
-      <p className="rating">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
-      <p className="price">R$ {price.toFixed(2)}</p>
-      <div className="buttons">
-        <Link to="/products/123/checkout" className="btn-icon">
+      <p className={styles["rating"]}>&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+      <p className={styles["price"]}><span>R$</span> {price.toFixed(2)}</p>
+      <div className={styles["buttons"]}>
+        <Link to="/products/123/checkout" className={styles["btn-icon"]}>
         <span>Comprar Agora</span>
         <FaMoneyBill/>
         </Link>
-        <button className="btn-icon add-to-cart"><span>Adicionar ao carrinho</span><FaCartShopping /></button>
+        <button className={`${styles["btn-icon"]} ${styles["add-to-cart"]}`}>
+          <span>Adicionar ao carrinho</span>
+          <FaCartShopping />
+        </button>
       </div>
     </div>
   );
